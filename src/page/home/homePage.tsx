8360fd4a27cdf5c33b52663146fb32e505b25ce6
Ian Greenough <ian@sutra.fit>
2022-05-25 11:54:50 +0200

    [ENG-7] Adds Styled Components

--- src/page/home/homePage.tsx
+++ src/page/home/homePage.tsx
@@ -1,13 +1,19 @@
+import styled from "styled-components";
+
 import { TextButton } from "../../component";
 
 type HomePageProps = {
   onButtonClick: () => void;
 };
 
+const Hero = styled.div`
+  font-size: 4rem;
+`;
+
 const HomePage = ({ onButtonClick }: HomePageProps) => {
   return (
     <>
-      <div>HOME</div>
+      <Hero>HOME</Hero>
       <TextButton label="Click Me" onClick={onButtonClick} />
     </>
   );
